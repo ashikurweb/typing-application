a69ee12104a714924a040afd34ef103659091714
ashikurweb <ashikurrahman7194@gmail.com>
2025-05-26 00:23:36 +0600

    wip: all package install and code refactoring

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,29 @@
 import './bootstrap';
+import '../css/app.css';
+
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m.js';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+import VueApexCharts from "vue3-apexcharts";
+
+const appName = import.meta.env.VITE_APP_NAME || 'Typing App';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .use(Antd)
+            .use(VueApexCharts)
+            .mount(el);
+    },
+    progress: {
+        color: '#3b82f6',
+    },
+});
