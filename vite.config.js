2f60c7125894f8940c5f38d29d70efcbeb787da6
ashikurweb <ashikurrahman7194@gmail.com>
2025-05-26 00:28:56 +0600

    wip: authentication packge setup breeze and inertia

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
+            ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
-         vue({
+        vue({
             template: {
                 transformAssetUrls: {
                     base: null,
@@ -17,6 +17,5 @@ export default defineConfig({
                 },
             },
         }),
-        tailwindcss(),
     ],
 });
